e21c02ea2aa77ce3eeffb430b77343702c6c89da
Med Amine bt <60607576+iifast2@users.noreply.github.com>
2024-01-17 11:53:09 +0100

    commit ~~
    commit ~~

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'The Flutter Way',
+      title: 'MABT-TECH',
       theme: ThemeData(
         brightness: Brightness.dark,
         elevatedButtonTheme: ElevatedButtonThemeData(
